f53d5a54d12f617ff1d8b83c9c14bc9a749d8252
Christopher Williams <c.j.williams717@gmail.com>
2017-05-25 17:38:41 -0400

    Changing Root Path from Mid to Ion

--- Mid/Assets/Scripts/ChrsUtils/EasingProperties.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using ChrsUtils.EasingEquations;
-
-[CreateAssetMenu (menuName = "Easing Properties")]
-public class EasingProperties : ScriptableObject
-{
-    [SerializeField] private Easing.FunctionType _Easing;
-    public Easing.Function MovementEasing { get { return Easing.GetFunctionWithTypeEnum(_Easing); }}
-}
